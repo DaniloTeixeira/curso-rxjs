9790097065e4b502ce4a25ed6840e16e2d730ad9
Danilo Rodrigues Teixeira <danilo.rt@outlook.com>
2023-02-27 00:41:38 -0300

    Criando função para BehaviorSubject

--- src/app/core/components/subject/subject.component.ts
+++ src/app/core/components/subject/subject.component.ts
@@ -23,6 +23,8 @@ export class SubjectComponent implements OnInit, OnDestroy {
 
   subject$ = new Subject<number>();
 
+  behaviorSubject$ = new BehaviorSubject<number>(0);
+
   destroyed$ = new Subject<void>();
 
   ngOnInit(): void {}
@@ -33,15 +35,16 @@ export class SubjectComponent implements OnInit, OnDestroy {
   }
 
   /*
-   Neste exemplo, estamos criando um Subject que emite valores do tipo number.
-   Estamos nos inscrevendo no Subject duas vezes, uma antes e outra depois de emitir o valor 1 usando o método next. Cada vez que um valor é emitido, os observadores recebem o valor correspondente.
- */
+    Neste exemplo, estamos criando um Subject que emite valores do tipo number.
+    Estamos nos inscrevendo no Subject duas vezes, uma antes e outra depois de emitir o valor 1 usando o método next. Cada vez que um valor é emitido, os observadores recebem o valor correspondente.
 
+    Observe que o primeiro observador recebeu os valores 1 e 2, enquanto o segundo observador só recebeu o valor 2.
+  */
   onSubject(): void {
     this.subject$
       .pipe(takeUntil(this.destroyed$))
       .subscribe((valueObs1) =>
-        console.log(`Observer 1 tem acesso ao valor: ${valueObs1}`)
+        console.log(`Observer Subject 1 tem acesso ao valor: ${valueObs1}`)
       );
 
     this.subject$.next(1);
@@ -49,9 +52,24 @@ export class SubjectComponent implements OnInit, OnDestroy {
     this.subject$
       .pipe(takeUntil(this.destroyed$))
       .subscribe((valueObs2) =>
-        console.log(`Observer 2 tem acesso ao valor: ${valueObs2}`)
+        console.log(`Observer Subject 2 tem acesso ao valor: ${valueObs2}`)
       );
 
     this.subject$.next(2);
+  }
+
+  /*
+    Neste exemplo, estamos criando um BehaviorSubject que emite valores do tipo number. Estamos definindo o valor inicial do BehaviorSubject como 0. Usando o método next, estamos emitindo o valor um, depois fazemos a incrição e em seguida fazemos a emissão de três valores: 2, 3 e 4. Então o BehaviorSubjec vai pegar o último valor emitido e todos os próximos, neste caso, ignorando o valor de inicialização que é o 0
+  */
+  onBehaviorSubject(): void {
+    this.behaviorSubject$.next(1);
+
+    this.behaviorSubject$
+      .pipe(takeUntil(this.destroyed$))
+      .subscribe(console.log);
+
+    this.behaviorSubject$.next(2);
+    this.behaviorSubject$.next(3);
+    this.behaviorSubject$.next(4);
   }
 }
