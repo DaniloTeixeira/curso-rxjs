5b5894fab6ece5fdd51ad4747e452e82d410beca
Danilo Rodrigues Teixeira <danilo.rt@outlook.com>
2023-02-27 00:28:14 -0300

    Criando função para testar o Subject

--- src/app/core/components/subject/subject.component.ts
+++ src/app/core/components/subject/subject.component.ts
@@ -1,46 +1,57 @@
-import { Component, OnInit } from '@angular/core';
-import { BehaviorSubject, ReplaySubject, Subject } from 'rxjs';
+import {
+  Component,
+  ElementRef,
+  OnDestroy,
+  OnInit,
+  ViewChild,
+} from '@angular/core';
+import {
+  BehaviorSubject,
+  ReplaySubject,
+  Subject,
+  AsyncSubject,
+  takeUntil,
+} from 'rxjs';
 
 @Component({
   selector: 'app-subject',
   templateUrl: './subject.component.html',
   styleUrls: ['./subject.component.scss'],
 })
-export class SubjectComponent implements OnInit {
-  ngOnInit(): void {
-    this.initSubject();
+export class SubjectComponent implements OnInit, OnDestroy {
+  @ViewChild('subjectBtn') subjectBtn!: ElementRef;
+
+  subject$ = new Subject<number>();
+
+  destroyed$ = new Subject<void>();
+
+  ngOnInit(): void {}
+
+  ngOnDestroy(): void {
+    this.destroyed$.next();
+    this.destroyed$.complete();
   }
 
-  private initSubject(): void {
-    /* Observable que não necessita de um valor para ser iniciado */
-    const subject = new Subject<number>();
+  /*
+   Neste exemplo, estamos criando um Subject que emite valores do tipo number.
+   Estamos nos inscrevendo no Subject duas vezes, uma antes e outra depois de emitir o valor 1 usando o método next. Cada vez que um valor é emitido, os observadores recebem o valor correspondente.
+ */
 
-    subject.subscribe({
-      next: (value) => console.log(value),
-    });
+  onSubject(): void {
+    this.subject$
+      .pipe(takeUntil(this.destroyed$))
+      .subscribe((valueObs1) =>
+        console.log(`Observer 1 tem acesso ao valor: ${valueObs1}`)
+      );
 
-    subject.next(10);
+    this.subject$.next(1);
 
-    /* Obervable que precisa de um valor quando é iniciado e emite o valor atual aos novos incritos */
-    const behaviorSubject = new BehaviorSubject(0);
+    this.subject$
+      .pipe(takeUntil(this.destroyed$))
+      .subscribe((valueObs2) =>
+        console.log(`Observer 2 tem acesso ao valor: ${valueObs2}`)
+      );
 
-    behaviorSubject.next(999);
-    behaviorSubject.next(888);
-    behaviorSubject.next(777);
-
-    /* O subscriber receberá apenas o último valor emitido */
-    behaviorSubject.subscribe({
-      next: (value) => console.log(value),
-    });
-
-    /* Não necessita de valor inicial e emite os últimos 3 valores para os novos inscritos */
-    const replaysubject = new ReplaySubject();
-
-    replaysubject.subscribe(console.log);
-
-    replaysubject.next(1);
-    replaysubject.next(2);
-    replaysubject.next(3);
-    replaysubject.next(4);
+    this.subject$.next(2);
   }
 }
