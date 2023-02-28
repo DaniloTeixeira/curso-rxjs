2edaebf61b893d4dd3552d41452c8fc69b20271c
Danilo Rodrigues Teixeira <danilo.rt@outlook.com>
2023-02-28 00:35:21 -0300

    Finalizando os tipos de subject

--- src/app/core/components/subject/subject.component.ts
+++ src/app/core/components/subject/subject.component.ts
@@ -21,10 +21,6 @@ import {
 export class SubjectComponent implements OnInit, OnDestroy {
   @ViewChild('subjectBtn') subjectBtn!: ElementRef;
 
-  subject$ = new Subject<number>();
-
-  behaviorSubject$ = new BehaviorSubject<number>(0);
-
   destroyed$ = new Subject<void>();
 
   ngOnInit(): void {}
@@ -36,40 +32,76 @@ export class SubjectComponent implements OnInit, OnDestroy {
 
   /*
     Neste exemplo, estamos criando um Subject que emite valores do tipo number.
-    Estamos nos inscrevendo no Subject duas vezes, uma antes e outra depois de emitir o valor 1 usando o método next. Cada vez que um valor é emitido, os observadores recebem o valor correspondente.
+    Estamos nos inscrevendo no Subject após a emissão de dois números feita pelo método next(). Cada vez que um valor é emitido, o observador recebe o valor correspondente. Observe que o Subject só recebe os valores que foram emitidos após a sua inscrição.
 
-    Observe que o primeiro observador recebeu os valores 1 e 2, enquanto o segundo observador só recebeu o valor 2.
   */
   onSubject(): void {
-    this.subject$
-      .pipe(takeUntil(this.destroyed$))
-      .subscribe((valueObs1) =>
-        console.log(`Observer Subject 1 tem acesso ao valor: ${valueObs1}`)
-      );
+    const subject$ = new Subject<number>();
 
-    this.subject$.next(1);
+    subject$.next(1);
+    subject$.next(2);
 
-    this.subject$
+    subject$
       .pipe(takeUntil(this.destroyed$))
       .subscribe((valueObs2) =>
-        console.log(`Observer Subject 2 tem acesso ao valor: ${valueObs2}`)
+        console.log(`Subject emitido após a inscrição: ${valueObs2}`)
       );
 
-    this.subject$.next(2);
+    subject$.next(3);
+    subject$.next(4);
   }
 
   /*
     Neste exemplo, estamos criando um BehaviorSubject que emite valores do tipo number. Estamos definindo o valor inicial do BehaviorSubject como 0. Usando o método next, estamos emitindo o valor um, depois fazemos a incrição e em seguida fazemos a emissão de três valores: 2, 3 e 4. Então o BehaviorSubjec vai pegar o último valor emitido e todos os próximos, neste caso, ignorando o valor de inicialização que é o 0
   */
   onBehaviorSubject(): void {
-    this.behaviorSubject$.next(1);
+    const behaviorSubject$ = new BehaviorSubject<number>(0);
 
-    this.behaviorSubject$
+    behaviorSubject$.next(0);
+    behaviorSubject$.next(1);
+
+    behaviorSubject$
       .pipe(takeUntil(this.destroyed$))
-      .subscribe(console.log);
+      .subscribe((value) => console.log(`BehaviorSubject: ${value}`));
 
-    this.behaviorSubject$.next(2);
-    this.behaviorSubject$.next(3);
-    this.behaviorSubject$.next(4);
+    behaviorSubject$.next(2);
+    behaviorSubject$.next(3);
+    behaviorSubject$.next(4);
+  }
+
+  /*
+    Neste exemplo, estamos criando um ReplaySubject que emite valores do tipo number. Estamos definindo o tamanho do buffer como 2. Cada vez que um valor é emitido, o observador recebe o valor correspondente, bem como os valores emitidos anteriormente, de acordo com o tamanho do buffer.
+  */
+  onReplaySubject(): void {
+    const replaySubject$ = new ReplaySubject<number>(2);
+
+    replaySubject$.next(1);
+    replaySubject$.next(2);
+    replaySubject$.next(3);
+    replaySubject$.next(4);
+    replaySubject$.next(5);
+
+    replaySubject$.pipe(takeUntil(this.destroyed$)).subscribe((value) => {
+      console.log(`ReplaySubject: ${value}`);
+    });
+
+    replaySubject$.next(6);
+  }
+
+  /*
+    Neste exemplo, estamos criando um AsyncSubject que emite valores do tipo string. Estamos nos inscrevendo duas vezes, uma antes e outra depois de emitir os valores usando o método next(). Estamos completando o AsyncReplay com o método complete(). O subcriber do AsyncSubject recebe apenas o último valor emitido e apenas quando o Observable é completado.
+   */
+  onAsyncSubject(): void {
+    const asyncSubject$ = new AsyncSubject<string>();
+
+    asyncSubject$.next('Primeiro valor');
+    asyncSubject$.next('Segundo valor');
+    asyncSubject$.next('Terceiro valor');
+    asyncSubject$.next('Quarto Valor');
+    asyncSubject$.complete();
+
+    asyncSubject$.pipe(takeUntil(this.destroyed$)).subscribe((value) => {
+      console.log(`Último valor emitido pelo AsyncSubject: ${value}`);
+    });
   }
 }
