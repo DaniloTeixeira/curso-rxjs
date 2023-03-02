37fd69f7f56279ffc4b2417c58cc436aa9eda22a
Danilo Rodrigues Teixeira <danilo.rt@outlook.com>
2023-03-01 22:19:22 -0300

    Criando descrição do interval

--- src/app/core/components/creation-operators/operators/interval/interval.component.ts
+++ src/app/core/components/creation-operators/operators/interval/interval.component.ts
@@ -1,23 +1,47 @@
-import { Component, OnInit } from '@angular/core';
-import { interval } from 'rxjs';
+import {
+  AfterViewInit,
+  Component,
+  ElementRef,
+  OnDestroy,
+  OnInit,
+  ViewChild,
+} from '@angular/core';
+import { fromEvent, interval, Subject, takeUntil } from 'rxjs';
 
 @Component({
   selector: 'app-interval',
   templateUrl: './interval.component.html',
   styleUrls: ['./interval.component.scss'],
 })
-export class IntervalComponent implements OnInit {
-  ngOnInit(): void {
+export class IntervalComponent implements AfterViewInit, OnDestroy {
+  @ViewChild('btnStart') btnStart!: ElementRef;
+  @ViewChild('btnStop') btnStop!: ElementRef;
+
+  destroyed$ = new Subject<void>();
+
+  ngAfterViewInit(): void {
     this.intervalOperator();
   }
 
+  ngOnDestroy(): void {
+    this.destroyed$.next();
+    this.destroyed$.complete();
+  }
+
   intervalOperator(): void {
-    const obsNumber$ = interval(1000);
+    const startButton$ = fromEvent(this.btnStart.nativeElement, 'click');
+    const stopButton$ = fromEvent(this.btnStop.nativeElement, 'click');
 
-    const subscription$ = obsNumber$.subscribe(console.log);
+    startButton$.pipe(takeUntil(this.destroyed$)).subscribe(() => {
+      const interval$ = interval(1000).pipe(takeUntil(stopButton$));
 
-    setTimeout(() => {
-      subscription$.unsubscribe();
-    }, 6000);
+      interval$.subscribe((value) =>
+        console.log(`Valor do interval: ${value}`)
+      );
+    });
+
+    stopButton$
+      .pipe(takeUntil(this.destroyed$))
+      .subscribe(() => console.log('Interval finalizado'));
   }
 }
